acc5285bb1cd1140eb75632f6c1f7295f1ef36cc
root <root@ip-172-31-15-189.ec2.internal>
2026-07-08 05:31:08 +0000

    Student file

---
+++ student.java
@@ -0,0 +1,17 @@
+public class Student {
+	public static void main(String[] args) {
+		int rollno=26;
+		String sname="Ajay";
+			String course="PYTHON";
+			System.out.println("LEARN HERE AND LEAD ANYWHERE!!);
+			System.out.println("My name is: " +sname);
+			System.out.println("My course is: " +course);
+			System.out.println("My Roll Number is: " +rollno);
+			System.out.println("====================");
+			System.out.println("WELCOME TO JENKINS");
+	}
+}
+
+
+
+
